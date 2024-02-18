3b999747fa6b97d723cd76b4ea430b1913bfbcfb
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-02-18 23:46:29 +0300

    modified code to include cli

--- models.py
+++ models.py
@@ -17,6 +17,47 @@ class Book(Base):
     pages_count = Column(Integer())
     rating = Column(Integer())
 
+    genre_id = Column(Integer(), ForeignKey('genres.id'))
+    genre = relationship("Genre", back_populates="books")
+
+    author_id = Column(Integer(), ForeignKey('authors.id'))
+    author = relationship("Author", back_populates="books")
+
+    def __repr__(self):
+        return f'Book(id={self.id},' + \
+               f'title={self.title},' + \
+               f'author={self.author},' + \
+               f'isbn={self.isbn},' + \
+               f'publication_date={self.publication_date},' + \
+               f'description={self.description},' + \
+               f'publisher={self.publisher},' + \
+               f'language={self.language},' + \
+               f'pages_count={self.pages_count},' + \
+               f'rating={self.rating})'
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    best_seller = Column(String())
+    books = relationship("Book", back_populates="author")
+
+    def __repr__(self):
+        return f'Author(id={self.id},' + \
+               f'name={self.name},' + \
+               f'best_seller={self.best_seller})'
+
+class Genre(Base):
+    __tablename__ = 'genres'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    books = relationship("Book", back_populates="genre")
+
+    def __repr__(self):
+        return f'Genre(id={self.id},' + \
+               f'name={self.name})'
+
+
 #create engine
 engine = create_engine('sqlite:///library.db')
 Base.metadata.create_all(engine)
